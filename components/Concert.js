a3788193caf7c8bb3963f2cf88b773165385a3f8
ThibaultBernard <bernard.thibault2209@gmail.com>
2022-01-04 17:44:14 +0100

    Concerts toevoegen

---
+++ components/Concert.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import SbEditable from "storyblok-react"
+import { render } from "storyblok-rich-text-react-renderer"
+import styles from "../styles/Concert.module.scss"
+import { getData } from "../utils/storyblok"
+import RelatedItemGallerySmall from "./RelatedItemGallerySmall"
+import RelatedItemGallery from "./RelatedItemGallery"
+import InPageSlideshow from "./InPageSlideshow"
+import SmallCardList from "./SmallCardList"
+
+
+
+const Concert = ({ data, level }) => {
+  var locale = 'en';
+  //enriching data
+  if (level === 'data') {
+    locale = data.story.lang;
+    var content = data.story.content;
+  } else {
+    var content = data;
+  }
+
+
+  return (
+    <SbEditable content={content} key={content._uid}>
+      <main>
+        {/* <div className={[styles.movie, styles.test].join(' ')}> */}
+        <div className={styles.Concert}>
+          <h1 className={styles.title}>
+            {content.title}
+          </h1>
+           <div className={styles.description}>
+            {render(content.description)}
+          </div>
+        </div>
+      </main>
+    </SbEditable>
+  )
+}
+
+export default Concert
