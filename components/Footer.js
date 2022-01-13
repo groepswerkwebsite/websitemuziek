e8b58d54c8f83c03fce7012c9d5e2621747c13ae
ThibaultBernard <bernard.thibault2209@gmail.com>
2022-01-13 22:25:18 +0100

    logo footer delete

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,8 @@ const Footer = () => {
       <div className={styles.footer}>
 
         <div className="">
-            <img
-            src="https://a.storyblok.com/f/133261/3039x582/a60d166ec2/logo-colored-full.png/m/200x0"
-            alt="Music is life itself"
-            className=""
-            />
+          ~Music is life itself~
+            
         </div>
         <p>The true beauty of music is that it connects people. It carries a message, and we, the musicians, are the messengers.</p>
       </div>
