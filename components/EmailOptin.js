5fdf972347d572d4ffaa7d9f8edc35826699d9f8
ThibaultBernard <bernard.thibault2209@gmail.com>
2022-01-14 14:55:25 +0100

    EmailOptin change

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {formstate&&<iframe width="540" height="305" src="https://cb31df7e.sibforms.com/serve/MUIEANer0QRiSNj97_jfDHEuvA0RzLEwOH3AXStPGw3_0rO3k-7rBb4xO78cgiotL6CzSuGZto8lS7gFstEzuII1yN6bJdo6SW0UtN7rrTdzSkoFFNIw7edz-C7eDpCS9ySojlbsoBdO2fU_Lej4qM1lPUntIC2Cp9kRBLaM6DJgMS4G7bR_pZxYy6ZaGARegVShAPmEdtyX5Ht0" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
     </div>
   );
 };
